7af166cbdc39cf0c794fa8f374219f1ce0c99592
isma2020 <65232463+isma2020@users.noreply.github.com>
2020-05-12 13:32:42 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// Définit les contraintes pour le flux vidéo 
+var contraintes = {video: {facesMode: "user"}, audio: false};
+
+// Définissez les constantes
+const cameraView = document.querySelector ("#camera-view"), 
+      cameraOutput = document.querySelector ("#camera-output"), 
+      cameraSensor = document.querySelector ("#camera-sensor"), 
+      cameraTrigger = document. querySelector ("#camera-trigger")
+	  
+// Accédez à la caméra de l'appareil et
+//diffusez vers la fonction cameraView	  
+function cameraStart () { 
+    navigator.mediaDevices 
+        .getUserMedia (contraintes) 
+        .then (function (stream) { 
+        track = stream.getTracks () [0]; 
+        cameraView.srcObject = stream; 
+    }) 
+    .catch (function (error) { 
+        console. error ("Oups. Quelque chose est cassé.", erreur); 
+    }); 
+}
+
+// Prenez une photo lorsque cameraTrigger est tapé 
+cameraTrigger.onclick = function () { 
+    cameraSensor.width = cameraView.videoWidth; 
+    cameraSensor.height = cameraView.videoHeight; 
+    cameraSensor.getContext ("2d"). drawImage (cameraView, 0, 0); 
+    cameraOutput.src = cameraSensor.toDataURL ("image/webp"); 
+    cameraOutput.classList.add ("pris"); 
+};
+
+// Démarre le flux vidéo lorsque la fenêtre charge 
+window.addEventListener ("charger", cameraStart, false);
+
+
